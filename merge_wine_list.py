2ea42f6f19d448bac3d359ad460a422b77301f78
Limgabi <gabi520@naver.com>
2022-10-09 11:34:47 +0900

    Update merge_wine_list.py

--- merge_wine_list.py
+++ merge_wine_list.py
@@ -3,14 +3,13 @@ import pandas as pd
 
 address = 'C:/Users/limga/OneDrive/Desktop/data/blogCrawling/'
 
-#wine_white = pd.read_csv('wine_white.csv', encoding='utf-8')
-#wine_sparkle = pd.read_csv('wine_sparkle.csv', encoding='utf-8')
+wine_white = pd.read_csv('wine_white.csv', encoding='utf-8')
+wine_sparkle = pd.read_csv('wine_sparkle.csv', encoding='utf-8')
 wine_rose = pd.read_csv('wine_rose.csv', encoding='utf-8')
 wine_etc = pd.read_csv('wine_etc.csv', encoding='utf-8')
 
 df = pd.concat(
-  #[wine_white, wine_rose, wine_sparkle, wine_etc]
-  [ wine_rose, wine_etc]
+	[wine_white, wine_rose, wine_sparkle, wine_etc]
 )
 
 
